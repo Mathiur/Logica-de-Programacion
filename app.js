14a6bf1256493ca3a03ba2f56e2d98960d95b48c
Mateo <mateopinzon19@gmail.com>
2025-08-01 23:13:57 -0500

    se modifico el archivo js con una mejor optimizacion

---
+++ app.js
@@ -0,0 +1,30 @@
+//Variables
+let numeroSecreto = Math.floor(Math.random()*10);
+let numeroUsuario = 0;
+let intentos = 1;
+let maximoIntentos = 3;
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+    console.log(typeof(numeroUsuario));
+
+    //Este código realiza
+    //la comparación
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario} con ${intentos} ${intentos == 1 ?"intento" : "intentos"}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert("El numero es menor");
+        } else {
+            alert("El numero es mayor");
+        }
+        intentos++;
+        if (intentos >maximoIntentos) {
+            alert(`Llegaste al numero maximo de ${maximoIntentos} intentos permitidos`);
+            break;
+        }
+        //La condición no se cumplió
+        // alert('Lo siento, no acertaste el número');
+    }
+} 
+
