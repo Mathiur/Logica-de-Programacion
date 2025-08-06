d775a869ddb086d1fb84787f846ae30cfedcdb29
Mateo <mateopinzon19@gmail.com>
2025-08-05 22:42:08 -0500

    SE modifican los archivos y se solucionan el Desafio-2 de curso de funciones

--- js-curso-2-main/app.js
+++ js-curso-2-main/app.js
@@ -5,18 +5,6 @@ function buttonConsole(params) {
     console.log("El botón fue clicado");
 }
 
-function buttonPrompt(params) {
-    let question = prompt("¿Escribe el nombre de una ciudad de Brasil");
-    alert("Estuve en " + question + " y me acordé de ti.");
-}
 
-function btnAlert(params) {
-    alert("Yo amo JS");
-}
 
-function btnSuma(params) {
-    let num1 = parseInt(prompt("ingrese un numero para operar"));
-    let num2 = parseInt(prompt("ingrese un segundo numero para operar"));
-    let resultado = num1 + num2;
-    alert(`El resultado de la suma de los numeros ${num1} + ${num2} es ${resultado}`);
-}
+
--- js-curso-2-main/desafiosCurso2/Desafio-1.md
+++ js-curso-2-main/desafiosCurso2/Desafio-1.md
@@ -1,3 +1,5 @@
+<h1>Desafio 1 : Hora de Practicar</h1>
+
 1. Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
 
 ```javascript
